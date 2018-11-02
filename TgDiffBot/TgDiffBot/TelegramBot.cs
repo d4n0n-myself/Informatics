d42ef8465fc96fb78d4d5244b20525cffa36e3cd
d4n0n_myself <danon.sibaev@gmail.com>
2018-11-02 20:58:58 +0300

    gitignored + minor fixes

---
+++ TgDiffBot/TgDiffBot/TelegramBot.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+
+namespace TgDiffBot
+{
+    public static class TelegramBot
+    {
+        private static ITelegramBotClient BotClient { get; set; }
+
+        private const string ApiToken = "666534499:AAFuj7s8Q31cbi_cEbYJAV9IzAzQO2JYKls";
+
+        public static void Main(params string[] args)
+        {
+            Console.WriteLine("Enable proxy? Y/N");
+            var key = Console.ReadKey();
+            Console.WriteLine("\n");
+
+            BotClient = key.Key == ConsoleKey.Y
+                ? Proxy.GetBotWithSocksProxy(ApiToken)
+                : new TelegramBotClient(ApiToken);
+
+            var me = GetBot();
+
+            Console.WriteLine(
+                $"Hello, World! I am user {me.Id} and my name is {me.FirstName}."
+            );
+
+            BotClient.OnMessage += Bot_OnMessage;
+            BotClient.StartReceiving();
+
+            while (true)
+                Thread.Sleep(int.MaxValue);
+        }
+        
+        private static User GetBot() => BotClient.GetMeAsync().Result;
+        
+        private static async void Bot_OnMessage(object sender, MessageEventArgs e)
+        {
+            var chatId = e.Message.Chat.Id;
+            Console.WriteLine($"Received message in {chatId}");
+            var result = ProvideResponseFromLibrary(e.Message.Text);
+            await BotClient.SendTextMessageAsync(chatId, result);
+        }
+
+        private static string ProvideResponseFromLibrary(string inputQueryParameter)
+        {
+            var url = $"http://localhost:1234/?Expression={inputQueryParameter}";
+            var request = WebRequest.Create(url);
+            var response = request.GetResponse();
+            var buffer = Encoding.ASCII.GetBytes(response.ToString());
+            return buffer.Length != 0 ? buffer.ToString() : string.Empty;
+        }
+    }
+}
