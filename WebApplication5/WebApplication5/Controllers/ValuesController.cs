cac65ac8bc43c6bc97b865cf7e35ac3ef18218c4
d4n0n_myself <danon.sibaev@gmail.com>
2019-02-09 13:11:13 +0300

    form play

---
+++ WebApplication5/WebApplication5/Controllers/ValuesController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication5.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ValuesController : Controller
+	{
+		// GET api/values
+		[HttpGet]
+		public ActionResult<IEnumerable<string>> Get()
+		{
+			return new string[] {"value1", "value2"};
+		}
+
+		// GET api/values/5
+		[HttpGet("{id}")]
+		public ActionResult<string> Get(int id)
+		{
+			return "value";
+		}
+
+		// POST api/values
+		[HttpPost]
+		public void Post([FromBody] string value)
+		{
+		}
+
+		// PUT api/values/5
+		[HttpPut("{id}")]
+		public void Put(int id, [FromBody] string value)
+		{
+		}
+
+		// DELETE api/values/5
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+		}
+
+		[HttpPost()]
+		[ActionName("Add")]
+		public void AddNote(string title, string text)
+		{ 
+			System.IO.File.WriteAllText($"texts/{title}.txt", text);
+		}
+	}
+}
---
+++ WebApplication5/WebApplication5/Startup.cs
@@ -0,0 +1,112 @@
+﻿using System.IO;
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebApplication5.Controllers;
+
+namespace WebApplication5
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			//services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseHsts();
+			}
+
+			//app.UseHttpsRedirection();
+			//app.UseMvc();
+
+			app.Map("/Add", builder => builder.Run(async context =>
+			{
+				using (var streamWriter = new StreamWriter(context.Response.Body))
+				{
+					var query = context.Request.Query;
+					var text = query["text"];
+					var title = query["title"];
+					
+					using (var controller = new ValuesController())
+					{
+						controller.AddNote(title, text);
+					}
+					
+					await streamWriter.WriteAsync("ti sozdal");
+				}
+			}));
+			
+			app.Map("/Home", builder => builder.Run(async context =>
+			{
+				var htmlPage = File.ReadAllText("Home.html");
+				htmlPage = htmlPage.Replace("List", GetFiles());
+				using (var streamWriter = new StreamWriter(context.Response.Body))
+				{
+					await streamWriter.WriteAsync(htmlPage);
+				}
+			}));
+
+			//app.Map("/texts", builder => builder.Run(async context => { }));
+			
+			app.MapWhen(context => context.Request.Path.StartsWithSegments(new PathString("/texts")), 
+				builder => builder.Run(async httpContext =>
+				{
+					var title = httpContext.Request.Query["name"];
+					var text = File.Exists("texts/" + title + ".txt") ? File.ReadAllText("texts/" + title + ".txt") : "";
+					await httpContext.Response.WriteAsync(text);
+				}) );
+
+			app.Map("/Files", builder => builder.Run(async context =>
+			{
+				using (var str = new StreamWriter(context.Response.Body))
+				{
+					var stream = new StreamWriter("/files");
+					var formFile = context.Request.Form.Files["file"].
+					await str.WriteAsync("234567890");
+				}
+			}));
+			
+			app.Run(async context =>
+			{
+				context.Response.StatusCode = 308;
+				context.Response.Headers["Location"] = "/Home";
+			});
+		}
+
+		private static string GetFiles()
+		{
+			var directoryInfo = new DirectoryInfo("texts");
+			var files = directoryInfo.GetFiles();
+
+			var sb = new StringBuilder();
+			sb.Append("<ul>");
+			foreach (var file in files)
+			{
+				var replace = file.Name.Replace(".txt","");
+				sb.Append($@"<li><a href=""texts?name={replace}"">{replace}</a></li>");
+			}
+			sb.Append("</ul>");
+			return sb.ToString();
+		}
+	}
+}
